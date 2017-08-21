8e4a8fee1be77bc657a3c62407ece1fb4997a445
matthewcornell <matt@matthewcornell.org>
2017-08-21 12:26:17 -0400

    added placeholder for project_visualizations.html

--- forecast_app/urls.py
+++ forecast_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^project/(?P<pk>\d+)$', views.ProjectDetailView.as_view(), name='project-detail'),
+    url(r'^project/(?P<pk>\d+)/visualizations$', views.project_visualizations, name='project-visualizations'),
     url(r'^model/(?P<pk>\d+)$', views.ForecastModelDetailView.as_view(), name='forecastmodel-detail'),
     url(r'^forecast/(?P<pk>\d+)$', views.ForecastDetailView.as_view(), name='forecast-detail'),
 ]
--- forecast_app/views.py
+++ forecast_app/views.py
@@ -5,11 +5,30 @@ from forecast_app.models import Project, ForecastModel, Forecast
 
 
 def index(request):
+    """
+    View function for home page of site.
+    """
     projects = Project.objects.all()
     return render(
         request,
         'index.html',
         context={'projects': projects},
+    )
+
+
+def project_visualizations(request, pk):
+    """
+    View function to render various visualizations for a particular project.
+
+    :param request:
+    :param pk:
+    :return:
+    """
+    project = Project.objects.get(pk=pk)
+    return render(
+        request,
+        'project_visualizations.html',
+        context={'project': project},
     )
 
 
