5afd40a95e8043d3ba7082f01b8045418614c7d5
matthewcornell <matt@matthewcornell.org>
2017-08-28 15:07:24 -0400

    cleaned up mmwr utils, added Predict The District ones, got TimeZeros for the latter working

--- utils/make_example_projects.py
+++ utils/make_example_projects.py
@@ -1,13 +1,15 @@
-import datetime
 import os
 from pathlib import Path
 
 # set up django. must be done before loading models. requires: os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forecast_repo.settings")
 import django
 
+from utils.predict_the_disctrict_utils import start_date_for_biweek
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forecast_repo.settings")
 django.setup()
 
+from utils.mmwr_utils import end_date_2016_2017_for_mmwr_week
 from forecast_app.models import DataFile, Project, Target, TimeZero, ForecastModel, Forecast
 
 #
@@ -23,79 +25,6 @@ from forecast_app.models import DataFile, Project, Target, TimeZero, ForecastMod
 print('* deleting database...')
 for model_class in [DataFile, Project, Target, TimeZero, ForecastModel, Forecast]:
     model_class.objects.all().delete()
-
-#
-# --- MMWR utils ----
-#
-
-# https://ibis.health.state.nm.us/resource/MMWRWeekCalendar.html
-# MMWR Week -> ENDING Dates for MMWR Weeks (Weeks start on Sunday and ends on Saturday with this date)
-MMWR_WEEK_TO_2016_17_TUPLE = {  # {week_number: (2016 date, 2017 date), ...}
-    1: ('1/9/2016', '1/7/2017'),
-    2: ('1/16/2016', '1/14/2017'),
-    3: ('1/23/2016', '1/21/2017'),
-    4: ('1/30/2016', '1/28/2017'),
-    5: ('2/6/2016', '2/4/2017'),
-    6: ('2/13/2016', '2/11/2017'),
-    7: ('2/20/2016', '2/18/2017'),
-    8: ('2/27/2016', '2/25/2017'),
-    9: ('3/5/2016', '3/4/2017'),
-    10: ('3/12/2016', '3/11/2017'),
-    11: ('3/19/2016', '3/18/2017'),
-    12: ('3/26/2016', '3/25/2017'),
-    13: ('4/2/2016', '4/1/2017'),
-    14: ('4/9/2016', '4/8/2017'),
-    15: ('4/16/2016', '4/15/2017'),
-    16: ('4/23/2016', '4/22/2017'),
-    17: ('4/30/2016', '4/29/2017'),
-    18: ('5/7/2016', '5/6/2017'),
-    19: ('5/14/2016', '5/13/2017'),
-    20: ('5/21/2016', '5/20/2017'),
-    21: ('5/28/2016', '5/27/2017'),
-    22: ('6/4/2016', '6/3/2017'),
-    23: ('6/11/2016', '6/10/2017'),
-    24: ('6/18/2016', '6/17/2017'),
-    25: ('6/25/2016', '6/24/2017'),
-    26: ('7/2/2016', '7/1/2017'),
-    27: ('7/9/2016', '7/8/2017'),
-    28: ('7/16/2016', '7/15/2017'),
-    29: ('7/23/2016', '7/22/2017'),
-    30: ('7/30/2016', '7/29/2017'),
-    31: ('8/6/2016', '8/5/2017'),
-    32: ('8/13/2016', '8/12/2017'),
-    33: ('8/20/2016', '8/19/2017'),
-    34: ('8/27/2016', '8/26/2017'),
-    35: ('9/3/2016', '9/2/2017'),
-    36: ('9/10/2016', '9/9/2017'),
-    37: ('9/17/2016', '9/16/2017'),
-    38: ('9/24/2016', '9/23/2017'),
-    39: ('10/1/2016', '9/30/2017'),
-    40: ('10/8/2016', '10/7/2017'),
-    41: ('10/15/2016', '10/14/2017'),
-    42: ('10/22/2016', '10/21/2017'),
-    43: ('10/29/2016', '10/28/2017'),
-    44: ('11/5/2016', '11/4/2017'),
-    45: ('11/12/2016', '11/11/2017'),
-    46: ('11/19/2016', '11/18/2017'),
-    47: ('11/26/2016', '11/25/2017'),
-    48: ('12/3/2016', '12/2/2017'),
-    49: ('12/10/2016', '12/9/2017'),
-    50: ('12/17/2016', '12/16/2017'),
-    51: ('12/24/2016', '12/23/2017'),
-    52: ('12/31/2016', '12/30/2017'),
-    53: ('', ''),
-}
-
-
-def mmwr_week_to_end_date_2016_2017(mmwr_week):  # ex: 43
-    # assumes 40-52 = 2016, 2017 o/w
-    week_num_2016_17_tuple = MMWR_WEEK_TO_2016_17_TUPLE[mmwr_week]
-    m_d_y = week_num_2016_17_tuple[1 if mmwr_week < 40 else 0]  # column 0: 2016, 1: 2017
-    month = int(m_d_y.split('/')[0])
-    day = int(m_d_y.split('/')[1])
-    year = int(m_d_y.split('/')[2])
-    return datetime.date(year, month, day)
-
 
 #
 # ---- create the CDC Flu challenge (2016-2017) project and targets ----
@@ -136,10 +65,10 @@ for target_name, descr in (
     Target.objects.create(project=p, name=target_name, description=descr)
 
 # create the project's TimeZeros. b/c this is a CDC project, timezero_dates are all MMWR Week ENDING Dates as listed in
-# MMWR_WEEK_TO_2016_17_TUPLE. xx. note that the project has no version_dates
+# MMWR_WEEK_TO_YEAR_TUPLE. note that the project has no version_dates
 for mmwr_week in list(range(43, 53)) + list(range(1, 19)):  # [43, ..., 52, 1, ..., 18] for 2016-2017
     TimeZero.objects.create(project=p,
-                            timezero_date=str(mmwr_week_to_end_date_2016_2017(mmwr_week)),
+                            timezero_date=str(end_date_2016_2017_for_mmwr_week(mmwr_week)),
                             version_date=None)
 
 #
@@ -162,7 +91,7 @@ def add_forecasts_to_model(forecast_model, kot_model_dir_name):
 
     for csv_file in [csv_file for csv_file in kot_model_dir.glob('*.csv')]:  # 'EW1-KoTstable-2017-01-17.csv'
         mmwr_week = csv_file.name.split('-')[0].split('EW')[1]  # re.split(r'^EW(\d*).*$', csv_file.name)[1]
-        timezero_date = mmwr_week_to_end_date_2016_2017(int(mmwr_week))
+        timezero_date = end_date_2016_2017_for_mmwr_week(int(mmwr_week))
         time_zero = fm.time_zero_for_timezero_date_str(timezero_date)
         if not time_zero:
             raise RuntimeError("no time_zero found for timezero_date={}. csv_file={}, mmwr_week={}".format(
@@ -262,7 +191,7 @@ TEN_BIWEEK_DESCR = "The number of reported cases in each of the following 10 biw
                    "future."
 TEN_BIWEEK_TARGETS = [("The number of reported cases in biweek {}".format(biweek_num + 1), TEN_BIWEEK_DESCR) for
                       biweek_num in range(10)]
-for target_name, descr in TEN_BIWEEK_TARGETS + [
+for target_name, descr in TEN_BIWEEK_TARGETS + [  # TODO use exact target names to match data files' 'Target' columns
     ('The total number of reported cases for that calendar year',
      "Unlike the biweekly incidence targets, this target is a single scalar value; the target does not change "
      "throughout the year."),
@@ -275,11 +204,20 @@ for target_name, descr in TEN_BIWEEK_TARGETS + [
 ]:
     Target.objects.create(project=p, name=target_name, description=descr)
 
-# TODO: create the project's TimeZeros
-# for mmwr_week in list(range(43, 53)) + list(range(1, 19)):  # [43, ..., 52, 1, ..., 18] for 2016-2017
-#     TimeZero.objects.create(project=p,
-#                             timezero_date=str(mmwr_week_to_end_date_2016_2017(mmwr_week)),
-#                             version_date=None)
+#
+# create the project's TimeZeros. recall from http://reichlab.io/guidelines.html :
+# - forecasts for each target must be submitted for each biweek in 2008 through 2013 (6 years)
+# - 6 training set years * 26 biweeks/year = 156 separate files
+# - file naming convention: "BW13-2016-TeamKCDE.csv"
+#   = uses biweek 13 surveillance data from 2016 (BW13-2016 is the latest biweek and year of data used in the forecast)
+#   = TeamKCDE is the abbreviated name of the team making the submission
+#
+
+for training_year in range(2008, 2013 + 1):
+    for biweek in range(26):
+        TimeZero.objects.create(project=p,
+                                timezero_date=str(start_date_for_biweek(biweek, training_year)),
+                                version_date=None)
 
 #
 # ---- create the ForecastModels and their Forecasts ----
---
+++ utils/mmwr_utils.py
@@ -0,0 +1,35 @@
+import csv
+import datetime
+
+# https://ibis.health.state.nm.us/resource/MMWRWeekCalendar.html
+# MMWR Week -> ENDING Dates for MMWR Weeks (Weeks start on Sunday and ends on Saturday with this date)
+
+# 16 columns. header: MMWR_Week	2006_end_date	2007_end_date	2008_end_date	2009_end_date	2010_end_date	2011_end_date	2012_end_date	2013_end_date	2014_end_date	2015_end_date	2016_end_date	2017_end_date	2018_end_date	2019_end_date	2020_end_date
+from pathlib import Path
+
+MMWR_DATA_FILE = Path('~/IdeaProjects/forecast-repository/utils/mmwr-calendar-dates-2006-2020.csv').expanduser()
+
+
+def make_mmwr_week_to_year_tuple():
+    """
+    :return: a dict that maps MMWR week_number -> a 15-tuple with the columns above, excluding the first (MMWR_Week)
+    """
+    mmwr_week_to_year_tuple = {}
+    with open(MMWR_DATA_FILE) as input_csv_file_fp:
+        input_csv_reader = csv.reader(input_csv_file_fp, dialect=csv.excel_tab)
+        next(input_csv_reader, None)  # skip header
+        for row in input_csv_reader:
+            mmwr_week_to_year_tuple[int(row[0])] = row[1:]
+    return mmwr_week_to_year_tuple
+
+
+MMWR_WEEK_TO_YEAR_TUPLE = make_mmwr_week_to_year_tuple()
+
+
+def end_date_2016_2017_for_mmwr_week(mmwr_week):  # ex: 43. assumes 40-52 = 2016, 2017 o/w
+    week_num_2006_20_tuple = MMWR_WEEK_TO_YEAR_TUPLE[mmwr_week]
+    m_d_y = week_num_2006_20_tuple[11 if mmwr_week < 40 else 10]  # column 10: 2016, 11: 2017
+    year = int(m_d_y.split('-')[0])
+    month = int(m_d_y.split('-')[1])
+    day = int(m_d_y.split('-')[2])
+    return datetime.date(year, month, day)
