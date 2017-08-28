3e9bbe3e1bf64273f82d98b9119d7e08d7a58147
matthewcornell <matt@matthewcornell.org>
2017-08-28 11:33:43 -0400

    added Predict the District Challenge project and targets. tweawked admin

--- forecast_app/admin.py
+++ forecast_app/admin.py
@@ -5,7 +5,6 @@ from django.utils.html import format_html
 
 from forecast_app.models import DataFile, Project, TimeZero, Target, ForecastModel, Forecast
 
-
 #
 # minimal registration of Models
 #
@@ -22,19 +21,16 @@ admin.site.register(Forecast)
 # Project admin
 #
 
-class ForecastModelAdminLinkInline(admin.TabularInline):
+class ForecastModelInline(admin.TabularInline):
     model = ForecastModel
-    fields = ('admin_link',)
+    fields = ('name', 'description', 'admin_link')
     readonly_fields = ('admin_link',)
-    can_delete = False
     extra = 0
-
 
     def admin_link(self, instance):
         url = reverse('admin:{}_{}_change'.format(instance._meta.app_label, instance._meta.model_name),
                       args=(instance.id,))
         return format_html('<a href="{}">{}</a>', url, str(instance))
-
 
     # https://stackoverflow.com/questions/4143886/django-admin-disable-the-add-action-for-a-specific-model
     def has_add_permission(self, request):
@@ -54,9 +50,7 @@ class TimeZeroInline(admin.TabularInline):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    # inlines = [ForecastModelInline, TargetInline, TimeZeroInline]
-    inlines = [ForecastModelAdminLinkInline, TargetInline, TimeZeroInline]
-
+    inlines = [ForecastModelInline, TargetInline, TimeZeroInline]
 
     def get_form(self, request, obj=None, **kwargs):
         # make the description widget larger
@@ -79,7 +73,6 @@ class ForecastInline(admin.TabularInline):
 class ForecastModelAdmin(admin.ModelAdmin):
     inlines = [ForecastInline]
     fields = ('project', 'name', 'description', 'url', 'auxiliary_data')
-
 
     def get_form(self, request, obj=None, **kwargs):
         # make the description widget larger
--- utils/make_example_projects.py
+++ utils/make_example_projects.py
@@ -1,16 +1,14 @@
 import datetime
+import os
 from pathlib import Path
 
 # set up django. must be done before loading models. requires: os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forecast_repo.settings")
 import django
-import os
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forecast_repo.settings")
 django.setup()
 
 from forecast_app.models import DataFile, Project, Target, TimeZero, ForecastModel, Forecast
-
 
 #
 # ---- print and delete (!) all user objects ----
@@ -103,33 +101,38 @@ def mmwr_week_to_end_date_2016_2017(mmwr_week):  # ex: 43
 # ---- create the CDC Flu challenge (2016-2017) project and targets ----
 #
 
-print('* creating project and models...')
+print('* creating CDC Flu challenge project and models...')
+
+df = DataFile.objects.create(
+    location='https://github.com/reichlab/2016-2017-flu-contest-ensembles/tree/master/inst/submissions',
+    file_type='d')
 
 p = Project.objects.create(
     name='CDC Flu challenge (2016-2017)',
     description="Code, results, submissions, and method description for the 2016-2017 CDC flu contest submissions "
                 "based on ensembles.",
-    url='https://github.com/reichlab/2016-2017-flu-contest-ensembles')
+    url='https://github.com/reichlab/2016-2017-flu-contest-ensembles',
+    core_data=df)
 
 WEEK_AHEAD_DESCR = "One- to four-week ahead forecasts will be defined as the weighted ILINet percentage for the target week."
-for target_name, descr in [
-    ('Season onset',
-     "The onset of the season is defined as the MMWR surveillance week "
-     "(http://wwwn.cdc.gov/nndss/script/downloads.aspx) when the percentage of visits for influenza-like illness (ILI) "
-     "reported through ILINet reaches or exceeds the baseline value for three consecutive weeks (updated 2016-2017 "
-     "ILINet baseline values for the US and each HHS region will be available at "
-     "http://www.cdc.gov/flu/weekly/overview.htm the week of October 10, 2016). Forecasted 'onset' week values should "
-     "be for the first week of that three week period."),
-    ('Season peak week',
-     "The peak week will be defined as the MMWR surveillance week that the weighted ILINet percentage is the highest "
-     "for the 2016-2017 influenza season."),
-    ('Season peak percentage',
-     "The intensity will be defined as the highest numeric value that the weighted ILINet percentage reaches during " \
-     "the 2016-2017 influenza season."),
-    ('1 wk ahead', WEEK_AHEAD_DESCR),
-    ('2 wk ahead', WEEK_AHEAD_DESCR),
-    ('3 wk ahead', WEEK_AHEAD_DESCR),
-    ('4 wk ahead', WEEK_AHEAD_DESCR)]:
+for target_name, descr in (
+        ('Season onset',
+         "The onset of the season is defined as the MMWR surveillance week "
+         "(http://wwwn.cdc.gov/nndss/script/downloads.aspx) when the percentage of visits for influenza-like illness (ILI) "
+         "reported through ILINet reaches or exceeds the baseline value for three consecutive weeks (updated 2016-2017 "
+         "ILINet baseline values for the US and each HHS region will be available at "
+         "http://www.cdc.gov/flu/weekly/overview.htm the week of October 10, 2016). Forecasted 'onset' week values should "
+         "be for the first week of that three week period."),
+        ('Season peak week',
+         "The peak week will be defined as the MMWR surveillance week that the weighted ILINet percentage is the highest "
+         "for the 2016-2017 influenza season."),
+        ('Season peak percentage',
+         "The intensity will be defined as the highest numeric value that the weighted ILINet percentage reaches during " \
+         "the 2016-2017 influenza season."),
+        ('1 wk ahead', WEEK_AHEAD_DESCR),
+        ('2 wk ahead', WEEK_AHEAD_DESCR),
+        ('3 wk ahead', WEEK_AHEAD_DESCR),
+        ('4 wk ahead', WEEK_AHEAD_DESCR)):
     Target.objects.create(project=p, name=target_name, description=descr)
 
 # create the project's TimeZeros. b/c this is a CDC project, timezero_dates are all MMWR Week ENDING Dates as listed in
@@ -236,6 +239,60 @@ fm = ForecastModel.objects.create(
     auxiliary_data=df)
 
 add_forecasts_to_model(fm, 'sarima')
+
+#
+# ---- create the Predict the District Challenge project and targets ----
+#
+
+print('* creating Predict the District Challenge project and models...')
+
+df = DataFile.objects.create(
+    location='https://github.com/matthewcornell/split_kot_models_from_submissions/tree/master/kcde',
+    file_type='d')
+
+p = Project.objects.create(
+    name='Predict the District Challenge',
+    description="A Reich Lab challenge of predicting dengue fever in Thailand at the district level.",
+    url='http://reichlab.io/guidelines.html',
+    core_data=df)
+
+TEN_BIWEEK_DESCR = "The number of reported cases in each of the following 10 biweeks. If data is observed through " \
+                   "time t then forecasts for times t+1, …, t+10 will be handed in. If time t falls within 10 " \
+                   "biweeks of the end of the calendar year, still, the forecast should include 10 biweeks into the " \
+                   "future."
+TEN_BIWEEK_TARGETS = [("The number of reported cases in biweek {}".format(biweek_num + 1), TEN_BIWEEK_DESCR) for
+                      biweek_num in range(10)]
+for target_name, descr in TEN_BIWEEK_TARGETS + [
+    ('The total number of reported cases for that calendar year',
+     "Unlike the biweekly incidence targets, this target is a single scalar value; the target does not change "
+     "throughout the year."),
+    ('Peak number of reported cases in a single biweek for that calendar year',
+     "Unlike the biweekly incidence targets, this target is a single scalar value; the target does not change "
+     "throughout the year."),
+    ('The biweek in which the peak incidence will occur',
+     "This is the only target whose set of units are not based on case incidence. The predictive distribution here "
+     "is a discrete distribution that can take values between 1 and 26."),
+]:
+    Target.objects.create(project=p, name=target_name, description=descr)
+
+# TODO: create the project's TimeZeros
+# for mmwr_week in list(range(43, 53)) + list(range(1, 19)):  # [43, ..., 52, 1, ..., 18] for 2016-2017
+#     TimeZero.objects.create(project=p,
+#                             timezero_date=str(mmwr_week_to_end_date_2016_2017(mmwr_week)),
+#                             version_date=None)
+
+#
+# ---- create the ForecastModels and their Forecasts ----
+#
+
+for model_name, team_name in [('spatial model/HHH4', 'Harley'),
+                              ('KCDE variant?', 'Casey'),
+                              ('annual GAM?', 'Steve'),
+                              ('TSIR w/serotype?', 'Xi'),
+                              ('Prophet', 'facebook, Kristina'),
+                              ('SARIMA?, Google?', 'MS student'),
+                              ('Hierarchical, state space', '?')]:
+    fm = ForecastModel.objects.create(project=p, name=model_name, description="Team {}'s model.".format(team_name))
 
 #
 # ---- done ----
