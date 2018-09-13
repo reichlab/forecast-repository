3f3795b58a3ed0191916e60cc40ff84da7ddc0e1
matthewcornell <matt@matthewcornell.org>
2018-09-13 13:23:27 -0400

    added verbosity arg

--- utils/print_project_info.py
+++ utils/print_project_info.py
@@ -11,22 +11,28 @@ from forecast_app.models import Project
 
 
 @click.command()
-@click.option('--print_details', is_flag=True, default=False)
-def print_project_info_app(print_details):
+@click.argument('verbosity', type=click.Choice(['1', '2', '3']), default='1')
+def main(verbosity):
+    """
+    :param verbosity: increasing from 1 (minimal verbosity) to 3 (maximal)
+    """
     projects = Project.objects.order_by('name')
     click.echo("Users: {}".format(User.objects.all()))
 
     if len(projects) != 0:
         click.echo("Found {} projects: {}".format(len(projects), projects))
         for project in projects:
-            print_project_info(project, print_details)
+            print_project_info(project, int(verbosity))
     else:
         click.echo("<No Projects>")
 
 
-def print_project_info(project, print_details):
+def print_project_info(project, verbosity):
     click.echo("* {} {!r} {}. {} {}. {}".format(project, project.csv_filename, project.truth_data_qs().count(),
                                             project.owner, project.model_owners.all(), project.get_summary_counts()))
+    if verbosity == 1:
+        return
+
     click.echo("** Targets")
     for target in project.targets.all():
         click.echo("  {}".format(target))
@@ -39,7 +45,7 @@ def print_project_info(project, print_details):
     for timezero in project.timezeros.all():
         click.echo("  {}".format(timezero))
 
-    if not print_details:
+    if verbosity == 2:
         return
 
     click.echo("** ForecastModels {}".format(project.models.all()))
@@ -50,4 +56,4 @@ def print_project_info(project, print_details):
 
 
 if __name__ == '__main__':
-    print_project_info_app()
+    main()
