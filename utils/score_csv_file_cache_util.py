2dd88e1bc995127c92d77aa6cac7a058ee3afd57
matthewcornell <matt@matthewcornell.org>
2020-08-10 12:04:26 -0400

    added --project-pk option to score_csv_file_cache_util.py

--- utils/score_csv_file_cache_util.py
+++ utils/score_csv_file_cache_util.py
@@ -2,14 +2,16 @@ import logging
 
 import click
 import django
+import django_rq
+from django.shortcuts import get_object_or_404
 
 
 # set up django. must be done before loading models. NB: requires DJANGO_SETTINGS_MODULE to be set
 django.setup()
 
-from forecast_repo.settings.base import S3_BUCKET_PREFIX
+from forecast_repo.settings.base import S3_BUCKET_PREFIX, UPDATE_PROJECT_SCORE_CSV_FILE_CACHE_QUEUE_NAME
 from forecast_app.models import Project
-from forecast_app.models.score_csv_file_cache import enqueue_score_csv_file_cache_all_projs
+from forecast_app.models.score_csv_file_cache import _update_project_score_csv_file_cache_worker
 
 
 logging.getLogger().setLevel(logging.INFO)
@@ -44,12 +46,19 @@ def clear():
 
 
 @cli.command()
-def update():
+@click.option('--project-pk')
+def update(project_pk):
     """
-    A subcommand that enqueues updating all projects' ScoreCsvFileCache.
+    A subcommand that enqueues updating one or all projects' ScoreCsvFileCache.
+
+    :param project_pk: if a valid Project pk then only that project's models are updated. o/w defers to `model_pk` arg
     """
-    click.echo("enqueuing all projects' ScoreCsvFileCaches")
-    enqueue_score_csv_file_cache_all_projs()
+    projects = [get_object_or_404(Project, pk=project_pk)] if project_pk else Project.objects.all()
+    click.echo("enqueuing' ScoreCsvFileCaches")
+    for project in projects:
+        click.echo(f"- {project}")
+        queue = django_rq.get_queue(UPDATE_PROJECT_SCORE_CSV_FILE_CACHE_QUEUE_NAME)
+        queue.enqueue(_update_project_score_csv_file_cache_worker, project.pk)
     click.echo("enqueuing done")
 
 
