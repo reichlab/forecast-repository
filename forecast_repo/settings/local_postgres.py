941ba82b146be183dda5a53c1e2382eea31f48ec
Yuxin Huang <yuxinhuang@umass.edu>
2020-10-09 14:38:52 -0400

    update project settings

--- forecast_repo/settings/local_postgres.py
+++ forecast_repo/settings/local_postgres.py
@@ -11,7 +11,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'forecast_repo',
-        'USER': 'cornell',
+        'USER': 'yuxin',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
