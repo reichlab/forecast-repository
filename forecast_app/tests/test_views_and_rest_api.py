ff895a1829228337f545dc2a5e039b029ea824ad
matthewcornell <matt@matthewcornell.org>
2022-09-20 14:04:22 -0400

    viz authorization fixes

--- forecast_app/tests/test_views_and_rest_api.py
+++ forecast_app/tests/test_views_and_rest_api.py
@@ -190,10 +190,10 @@ class ViewsTestCase(TestCase):
             (reverse('project-forecasts', args=[str(self.private_project.pk)]), self.ONLY_PO_MO),
             (reverse('project-explorer', args=[str(self.public_project.pk)]), self.OK_ALL),
             (reverse('project-explorer', args=[str(self.private_project.pk)]), self.ONLY_PO_MO),
-            (reverse('project-viz', args=[str(self.public_project.pk)]), self.OK_ALL),
+            (reverse('project-viz', args=[str(self.public_project.pk)]), self.ONLY_PO_MO_STAFF),
             (reverse('project-viz', args=[str(self.private_project.pk)]), self.ONLY_PO_MO),
-            (reverse('project-viz-options-edit', args=[str(self.public_project.pk)]), self.ONLY_PO),
-            (reverse('project-viz-options-edit', args=[str(self.private_project.pk)]), self.ONLY_PO),
+            (reverse('project-viz-options-edit', args=[str(self.public_project.pk)]), self.ONLY_PO_MO_STAFF),
+            (reverse('project-viz-options-edit', args=[str(self.private_project.pk)]), self.ONLY_PO_MO),
             (reverse('project-config', args=[str(self.public_project.pk)]), self.OK_ALL),
             (reverse('project-config', args=[str(self.private_project.pk)]), self.ONLY_PO_MO),
             (reverse('create-project-from-form', args=[]), self.ONLY_PO_MO),
@@ -472,43 +472,41 @@ class ViewsTestCase(TestCase):
                 self.assertEqual(exp_status, response.status_code)
 
 
-    def test_url_post_project_viz_options(self):
+    def test_url_post_project_viz_execute(self):
         """
-        Very similar to test_url_post_edit_project_from_file() - please see comments there.
+        Similar to test_url_post_edit_project_from_file() - please see comments there.
         """
-        for url_name in ['project-viz-options-edit', 'project-viz-options-execute']:
-            for proj_pk in [self.public_project.pk, self.private_project.pk]:
-                url = reverse(url_name, args=[str(proj_pk)])
+        url_name = 'project-viz-options-execute'
+        for proj_pk in [self.public_project.pk, self.private_project.pk]:
+            url = reverse(url_name, args=[str(proj_pk)])
 
-                self.client.logout()  # AnonymousUser
-                response = self.client.post(url)
-                self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+            self.client.logout()  # AnonymousUser
+            response = self.client.post(url)
+            self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
 
-                self.client.login(username=self.mo_user.username, password=self.mo_user_password)
-                response = self.client.post(url)
-                self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+            self.client.login(username=self.mo_user.username, password=self.mo_user_password)
+            response = self.client.post(url)
+            self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
 
-                self.client.login(username=self.non_staff_user.username, password=self.non_staff_user_password)
-                response = self.client.post(url)
-                self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+            self.client.login(username=self.non_staff_user.username, password=self.non_staff_user_password)
+            response = self.client.post(url)
+            self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
 
-                # we pass 'validateOnlyCheckbox' and 'optionsTextArea' to prevent project_viz_options_execute() from
-                # throwing an error like:
-                #   TypeError: the JSON object must be str, bytes or bytearray, not NoneType
-                # urlencode & content_type per https://stackoverflow.com/questions/50240315/django-apiclient-post-empty
-                self.client.login(username=self.po_user.username, password=self.po_user_password)
-                options = {'initial_target_var': 'ili_percent', 'initial_unit': 'US National', 'intervals': [0],
-                           'initial_checked_models': ['abbrev'], 'models_at_top': ['abbrev'], 'disclaimer': ''}
-                options_str = json.dumps(options)
-                post_data = {'validateOnlyCheckbox': 'off', 'optionsTextArea': options_str}
-                response = self.client.post(url, urlencode(post_data), content_type='application/x-www-form-urlencoded')
-                exp_status = status.HTTP_200_OK if url_name == 'project-viz-options-edit' \
-                    else status.HTTP_302_FOUND
-                self.assertEqual(exp_status, response.status_code)
+            # we pass 'validateOnlyCheckbox' and 'optionsTextArea' to prevent project_viz_options_execute() from
+            # throwing an error like:
+            #   TypeError: the JSON object must be str, bytes or bytearray, not NoneType
+            # urlencode & content_type per https://stackoverflow.com/questions/50240315/django-apiclient-post-empty
+            self.client.login(username=self.po_user.username, password=self.po_user_password)
+            options = {'initial_target_var': 'ili_percent', 'initial_unit': 'US National', 'intervals': [0],
+                       'initial_checked_models': ['abbrev'], 'models_at_top': ['abbrev'], 'disclaimer': ''}
+            options_str = json.dumps(options)
+            post_data = {'validateOnlyCheckbox': 'off', 'optionsTextArea': options_str}
+            response = self.client.post(url, urlencode(post_data), content_type='application/x-www-form-urlencoded')
+            self.assertEqual(status.HTTP_302_FOUND, response.status_code)
 
-                self.client.login(username=self.superuser.username, password=self.superuser_password)
-                response = self.client.post(url, urlencode(post_data), content_type='application/x-www-form-urlencoded')
-                self.assertEqual(exp_status, response.status_code)
+            self.client.login(username=self.superuser.username, password=self.superuser_password)
+            response = self.client.post(url, urlencode(post_data), content_type='application/x-www-form-urlencoded')
+            self.assertEqual(status.HTTP_302_FOUND, response.status_code)
 
 
     def test_delete_project_interactively(self):
